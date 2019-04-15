a1281f555c7d0cec4bd36d3a285d7ffd1a843990
Johnny Peterson <johnny.d.peterson@gmail.com>
2019-04-15 03:43:41 -0600

    working on reports

--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,5 @@ C195 - Software II - Advanced Java Concepts - WGU Project
 
 ## Assumptions:  
 * Changing the db is not ok but adding a view for reporting should be.
+
+* I assume the contact from the Data Base can be considered a consultant.
--- src/view/ReportsController.java
+++ src/view/ReportsController.java
@@ -2,17 +2,25 @@ package view;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import model.Appointment;
 import model.TypeCount;
 import util.DataBase;
 
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ResourceBundle;
 
 public class ReportsController implements Initializable {
@@ -26,12 +34,44 @@ public class ReportsController implements Initializable {
     @FXML
     private TableColumn<TypeCount, String> typesColumn;
 
+    @FXML
+    private TableView<?> scheduleTableView;
+
+    @FXML
+    private TableColumn<?, ?> startColumn;
+
+    @FXML
+    private TableColumn<?, ?> endColumn;
+
+    @FXML
+    private TableColumn<?, ?> titleColumn;
+
+    @FXML
+    private TableColumn<?, ?> typeColumn;
+
+    @FXML
+    private TableColumn<?, ?> CustomerColumn;
+
+    @FXML
+    private ComboBox<?> consultantComboBox;
+
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+    private final ZoneId zoneId = ZoneId.systemDefault();
+
+    @FXML
+    void handleComboBox(ActionEvent event) {
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setTypesTableView();
 
 
     }
+
+
+
 
     private ObservableList<TypeCount> getTypeReport() {
         ObservableList<TypeCount> typeObservableList = FXCollections.observableArrayList();
@@ -59,5 +99,56 @@ public class ReportsController implements Initializable {
         typesTableView.setItems(getTypeReport());
     }
 
+    public ObservableList<Appointment> getAppointmentList(String contact) {
+        ObservableList<Appointment> appointmentList = FXCollections.observableArrayList();
+        try {
+            PreparedStatement preparedStatement = DataBase.getConnection().prepareStatement(
+                    "SELECT * From appointment WHERE contact = ? ");
+            preparedStatement.setString(1, contact);
 
-}
+            ResultSet result = preparedStatement.executeQuery();
+
+
+            while (result.next()) {
+                Appointment appointment;
+
+                ZonedDateTime startLocal = localTime(result.getTimestamp("start"));
+                ZonedDateTime endTimeLocal = localTime(result.getTimestamp("end"));
+
+                appointment = new Appointment(result.getInt("appointmentId"),
+                        result.getInt("customerId"),
+                        result.getString("title"),
+                        result.getString("description"),
+                        result.getString("location"),
+                        result.getString("contact"),
+                        result.getString("url"),
+                        startLocal.format(dateTimeFormatter),
+                        endTimeLocal.format(dateTimeFormatter)
+                );
+                appointmentList.add(appointment);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+        public ZonedDateTime localTime(Timestamp timestamp) {
+
+            ZoneId zoneId = ZoneId.systemDefault();
+            ZonedDateTime zonedDateTime = timestamp.toLocalDateTime().atZone(ZoneId.of("UTC"));
+            ZonedDateTime localTime = zonedDateTime.withZoneSameInstant(zoneId);
+            return localTime;
+        }
+
+    private void setTableView(String user){
+
+    }
+
+    private  ObservableList<String> getContacts(){
+        
+    }
+
+
+
+    }
